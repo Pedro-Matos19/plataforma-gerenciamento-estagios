6a4a5a8c9062f7658b07ced29c7b335fd1bf3b21
joão henrique <joaolgg2estudos@gmail.com>
2026-02-16 21:55:59 -0300

    novo enum para o status da vaga e suas devidas mudanças em classe relacionadas

--- backend/src/main/java/com/ufape/estagios/repository/VagaRepository.java
+++ backend/src/main/java/com/ufape/estagios/repository/VagaRepository.java
@@ -7,12 +7,23 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.ufape.estagios.model.StatusDaVaga;
+import com.ufape.estagios.model.Usuario;
+import com.ufape.estagios.model.Vaga;
+
 @Repository
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
 
-    List<Vaga> findByEmpresaAndAtivaTrue(Usuario empresa);
 
-    List<Vaga> findByAtivaTrue();
+	@Query("SELECT v FROM Vaga v WHERE v.empresa = :empresa and v.status = 'EM_ABERTO'")
+    List<Vaga> findByEmpresaAndStatusIsEmAberto(@Param(value = "empresa") Usuario empresa);
+
+    List<Vaga> findByStatus(StatusDaVaga statusDaVaga);
 
     List<Vaga> findByEmpresa(Usuario empresa);
 }
