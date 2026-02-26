c0cd1339e9a13213117fbf209caa7ece50adbc70
joão henrique <joaolgg2estudos@gmail.com>
2026-02-25 23:44:07 -0300

    passando a lógica de autenticação para o service, tratando o erro de email repetido no registro e da autenticação no login

--- backend/src/main/java/com/ufape/estagios/exception/GlobalHandlerException.java
+++ backend/src/main/java/com/ufape/estagios/exception/GlobalHandlerException.java
@@ -2,6 +2,7 @@ package com.ufape.estagios.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,4 +18,14 @@ public class GlobalHandlerException {
 	public ResponseEntity<String> handlerAccessDeniedException(AccessDeniedException ex){
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
 	}
+	
+	@ExceptionHandler(EmailAlreadyExistsException.class)
+	public ResponseEntity<String> handlerEmailAlreadyExistsException(EmailAlreadyExistsException ex){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+	}
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<String> handlerBadCredentialsException(BadCredentialsException ex){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+	}
+	
 }
